2596a325381647d78959e857bba3592cfa548c1d
Techtitude Tribe <techtitudetribe@gmal.com>
2020-08-26 21:29:08 +0530

    Source Code

--- app/src/main/java/com/example/attachakki/HomeActivity.java
+++ app/src/main/java/com/example/attachakki/HomeActivity.java
@@ -13,6 +13,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
    CircleImageView setting;
+   CircleImageView profile;
+   CircleImageView add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,20 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
        setting=findViewById(R.id.settings);
+       profile=findViewById(R.id.img_contact);
+       add=findViewById(R.id.add_contact);
 
        setting.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
                Intent i=new Intent(HomeActivity.this, Setting.class);
+               startActivity(i);
+           }
+       });
+       profile.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i=new Intent(HomeActivity.this, Profile.class);
                startActivity(i);
            }
        });
