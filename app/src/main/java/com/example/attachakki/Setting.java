0bd4d1140bf722ccef2e629eb34a1cfc7b499ebd
Techtitude Tribe <techtitudetribe@gmal.com>
2020-09-30 17:17:11 +0530

    Source Code

--- app/src/main/java/com/example/attachakki/Setting.java
+++ app/src/main/java/com/example/attachakki/Setting.java
@@ -1,16 +1,24 @@
 package com.example.attachakki;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Locale;
+
 public class Setting extends AppCompatActivity {
-    Button btnLogout,edit;
+    Button btnLogout,edit,changelang;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
@@ -18,10 +26,15 @@ public class Setting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_setting);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
 
         btnLogout=findViewById(R.id.logout);
         edit= findViewById(R.id.edit_profile);
+        changelang=findViewById(R.id.changeLang);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +51,52 @@ public class Setting extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        changelang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showChangeLanguageDialog();
+            }
+        });
+    }
+
+    private void showChangeLanguageDialog() {
+     final String[] listItems={"English" , "हिंदी"};
+        AlertDialog.Builder mBuilder= new AlertDialog.Builder(Setting.this);
+        mBuilder.setTitle("Choose Language...");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+               if(i==0){
+                   //Hindi
+                    setLocale("hi");
+                    recreate();
+               }
+                else if(i==1){
+                    //English
+                    setLocale("en");
+                    recreate();
+                }
+
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog mDialog=mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale=new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config=new Configuration();
+        config.locale=locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor= getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang",lang);
+        editor.apply();
+    }
+    public void loadLocale(){
+        SharedPreferences prefs=getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language= prefs.getString("My_Lang", "");
+        setLocale(language);
     }
 }
